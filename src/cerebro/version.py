10a32cb93553b17ef0f8f993550b4b30da8c0c90
Luis Camargo <luiscamargo@Luiss-Mac-mini.local>
2026-02-19 00:35:16 -0800

    Fix version.py to use importlib.metadata for PEP 621

--- src/cerebro/version.py
+++ src/cerebro/version.py
@@ -1,10 +1,4 @@
-import toml
+from importlib.metadata import version
 
 
-def get_version():
-    with open("pyproject.toml", "r") as file:
-        pyproject = toml.load(file)
-    return pyproject["tool"]["poetry"]["version"]
-
-
-__version__ = get_version()
+__version__ = version("cerebro")
